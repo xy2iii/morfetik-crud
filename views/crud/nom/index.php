d57d1b1ab06350b386d7ae13d0366d92a49b7f5a
xy2_ <me@xy2.dev>
2020-06-02 14:55:38 +0200

    Update filter URLs

--- views/crud/nom/index.php
+++ views/crud/nom/index.php
@@ -28,6 +28,7 @@ CrudAsset::register($this);
             'id' => 'crud-datatable',
             'dataProvider' => $dataProvider,
             'filterModel' => $searchModel,
+            'filterUrl' => '@web/crud/nom',
             'pjax' => true,
             'pjaxSettings' => ['options' => ['enablePushState' => false]],
             'columns' => require(__DIR__ . '/_columns.php'),
